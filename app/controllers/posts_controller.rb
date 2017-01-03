7affcec085b124edd124c8f96e522e90883330a0
Joe Bernardi <joebernardi@hotmail.com>
2017-01-02 20:41:01 -0600

    create build comments for fix to view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,16 @@ class PostsController < ApplicationController
 	def new
 
 		@post = Post.new
+		@post.comments.build #why is this the solution?
 
 	end
+
+	def create
+
+		binding.pry
+
+	end
+
 
 
 end
